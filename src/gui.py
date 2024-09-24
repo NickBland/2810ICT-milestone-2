f2ba12f184c8fb90351e8a9866d63959a1aab44e
Nick Bland <nick.bland@nickbland.dev>
2024-09-24 20:07:32 +1000

    Add in rest of search form GUI elements

--- src/gui.py
+++ src/gui.py
@@ -28,7 +28,7 @@ class MyFrame3(wx.Frame):
             id=wx.ID_ANY,
             title=wx.EmptyString,
             pos=wx.DefaultPosition,
-            size=wx.Size(1102, 503),
+            size=wx.Size(1111, 558),
             style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL,
         )
 
@@ -50,23 +50,23 @@ class MyFrame3(wx.Frame):
 
         search_left_panel = wx.BoxSizer(wx.VERTICAL)
 
-        self.m_panel4 = wx.Panel(
+        self.nutrient_panel_top = wx.Panel(
             self.search_panel,
             wx.ID_ANY,
             wx.DefaultPosition,
             wx.DefaultSize,
             wx.TAB_TRAVERSAL,
         )
-        search_left_panel.Add(self.m_panel4, 1, wx.EXPAND | wx.ALL, 5)
+        search_left_panel.Add(self.nutrient_panel_top, 1, wx.EXPAND | wx.ALL, 5)
 
-        self.m_panel5 = wx.Panel(
+        self.nutrient_panel_bot = wx.Panel(
             self.search_panel,
             wx.ID_ANY,
             wx.DefaultPosition,
             wx.DefaultSize,
             wx.TAB_TRAVERSAL,
         )
-        search_left_panel.Add(self.m_panel5, 1, wx.EXPAND | wx.ALL, 5)
+        search_left_panel.Add(self.nutrient_panel_bot, 1, wx.EXPAND | wx.ALL, 5)
 
         search_bSizer.Add(search_left_panel, 1, wx.EXPAND, 5)
 
@@ -75,7 +75,7 @@ class MyFrame3(wx.Frame):
         )
 
         # Grid
-        self.search_results_grid.CreateGrid(5, 5)
+        self.search_results_grid.CreateGrid(2, 2)
         self.search_results_grid.EnableEditing(True)
         self.search_results_grid.EnableGridLines(True)
         self.search_results_grid.EnableDragGridSize(False)
@@ -87,6 +87,7 @@ class MyFrame3(wx.Frame):
         self.search_results_grid.SetColLabelAlignment(wx.ALIGN_CENTER, wx.ALIGN_CENTER)
 
         # Rows
+        self.search_results_grid.AutoSizeRows()
         self.search_results_grid.EnableDragRowSize(True)
         self.search_results_grid.SetRowLabelAlignment(wx.ALIGN_CENTER, wx.ALIGN_CENTER)
 
@@ -133,6 +134,290 @@ class MyFrame3(wx.Frame):
         )
         search_right_panel.Add(self.search_keyword_input, 0, wx.ALL | wx.EXPAND, 5)
 
+        self.search_filters_label = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Filters"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        self.search_filters_label.Wrap(-1)
+
+        self.search_filters_label.SetFont(
+            wx.Font(
+                16,
+                wx.FONTFAMILY_DEFAULT,
+                wx.FONTSTYLE_NORMAL,
+                wx.FONTWEIGHT_NORMAL,
+                False,
+                wx.EmptyString,
+            )
+        )
+
+        search_right_panel.Add(
+            self.search_filters_label, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5
+        )
+
+        self.search_filter_nutrients_label = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Nutrients:"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        self.search_filter_nutrients_label.Wrap(-1)
+
+        search_right_panel.Add(self.search_filter_nutrients_label, 0, wx.ALL, 5)
+
+        search_filter_nutrient_selectionChoices = []
+        self.search_filter_nutrient_selection = wx.Choice(
+            self.search_panel,
+            wx.ID_ANY,
+            wx.DefaultPosition,
+            wx.Size(200, -1),
+            search_filter_nutrient_selectionChoices,
+            0,
+        )
+        self.search_filter_nutrient_selection.SetSelection(0)
+        search_right_panel.Add(
+            self.search_filter_nutrient_selection,
+            0,
+            wx.ALL | wx.ALIGN_CENTER_HORIZONTAL,
+            5,
+        )
+
+        self.search_filter_range_label = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Nutrient Range:"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        self.search_filter_range_label.Wrap(-1)
+
+        search_right_panel.Add(self.search_filter_range_label, 0, wx.ALL, 5)
+
+        search_filter_range_box = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.search_filter_range_min_label = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Min"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            wx.ALIGN_RIGHT,
+        )
+        self.search_filter_range_min_label.Wrap(-1)
+
+        search_filter_range_box.Add(self.search_filter_range_min_label, 1, wx.ALL, 5)
+
+        self.search_filter_range_min = wx.TextCtrl(
+            self.search_panel,
+            wx.ID_ANY,
+            wx.EmptyString,
+            wx.DefaultPosition,
+            wx.Size(50, -1),
+            0,
+        )
+        search_filter_range_box.Add(self.search_filter_range_min, 0, wx.ALL, 5)
+
+        self.search_filter_range_mid_label = wx.StaticText(
+            self.search_panel, wx.ID_ANY, _("-"), wx.DefaultPosition, wx.DefaultSize, 0
+        )
+        self.search_filter_range_mid_label.Wrap(-1)
+
+        search_filter_range_box.Add(self.search_filter_range_mid_label, 0, wx.ALL, 5)
+
+        self.search_filter_range_max = wx.TextCtrl(
+            self.search_panel,
+            wx.ID_ANY,
+            wx.EmptyString,
+            wx.DefaultPosition,
+            wx.Size(50, -1),
+            0,
+        )
+        search_filter_range_box.Add(self.search_filter_range_max, 0, wx.ALL, 5)
+
+        self.search_filter_range_max_label = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Max"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        self.search_filter_range_max_label.Wrap(-1)
+
+        search_filter_range_box.Add(self.search_filter_range_max_label, 1, wx.ALL, 5)
+
+        search_right_panel.Add(
+            search_filter_range_box,
+            0,
+            wx.ALIGN_TOP | wx.ALL | wx.SHAPED | wx.ALIGN_CENTER_HORIZONTAL,
+            5,
+        )
+
+        self.search_filter_nutritional_level_label = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Nutritional Level:"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        self.search_filter_nutritional_level_label.Wrap(-1)
+
+        search_right_panel.Add(self.search_filter_nutritional_level_label, 0, wx.ALL, 5)
+
+        m_choice3Choices = []
+        self.m_choice3 = wx.Choice(
+            self.search_panel,
+            wx.ID_ANY,
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            m_choice3Choices,
+            0,
+        )
+        self.m_choice3.SetSelection(0)
+        search_right_panel.Add(
+            self.m_choice3, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5
+        )
+
+        self.search_filter_other_label = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Other:"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        self.search_filter_other_label.Wrap(-1)
+
+        search_right_panel.Add(self.search_filter_other_label, 0, wx.ALL, 5)
+
+        bSizer9 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.search_filter_highProtein = wx.CheckBox(
+            self.search_panel,
+            wx.ID_ANY,
+            _("High Protein"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        bSizer9.Add(self.search_filter_highProtein, 0, wx.ALL, 5)
+
+        self.search_filter_lowSugar = wx.CheckBox(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Low Sugar"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        bSizer9.Add(self.search_filter_lowSugar, 0, wx.ALL, 5)
+
+        search_right_panel.Add(bSizer9, 0, wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        self.m_staticline1 = wx.StaticLine(
+            self.search_panel,
+            wx.ID_ANY,
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            wx.LI_HORIZONTAL,
+        )
+        search_right_panel.Add(self.m_staticline1, 0, wx.EXPAND | wx.ALL, 5)
+
+        self.search_result_label = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Currently Selected:"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            wx.ALIGN_CENTER_HORIZONTAL,
+        )
+        self.search_result_label.Wrap(-1)
+
+        self.search_result_label.SetFont(
+            wx.Font(
+                16,
+                wx.FONTFAMILY_DEFAULT,
+                wx.FONTSTYLE_NORMAL,
+                wx.FONTWEIGHT_NORMAL,
+                False,
+                wx.EmptyString,
+            )
+        )
+
+        search_right_panel.Add(
+            self.search_result_label, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALL, 5
+        )
+
+        self.search_result_result = wx.StaticText(
+            self.search_panel,
+            wx.ID_ANY,
+            _("No Food Selected"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        self.search_result_result.Wrap(-1)
+
+        search_right_panel.Add(
+            self.search_result_result, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5
+        )
+
+        bSizer10 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.m_button13 = wx.Button(
+            self.search_panel,
+            wx.ID_ANY,
+            _("View Data"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        bSizer10.Add(self.m_button13, 1, wx.ALL, 5)
+
+        self.m_button14 = wx.Button(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Reset"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        bSizer10.Add(self.m_button14, 1, wx.ALL, 5)
+
+        search_right_panel.Add(bSizer10, 1, wx.EXPAND, 5)
+
+        bSizer11 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.m_button15 = wx.Button(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Add/Remove from Comparison"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        bSizer11.Add(self.m_button15, 1, wx.ALL, 5)
+
+        self.m_button16 = wx.Button(
+            self.search_panel,
+            wx.ID_ANY,
+            _("Exit"),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        bSizer11.Add(self.m_button16, 1, wx.ALL, 5)
+
+        search_right_panel.Add(bSizer11, 1, wx.EXPAND, 5)
+
         search_bSizer.Add(search_right_panel, 1, wx.EXPAND, 5)
 
         self.search_panel.SetSizer(search_bSizer)
@@ -163,5 +448,24 @@ class MyFrame3(wx.Frame):
 
         self.Centre(wx.BOTH)
 
+        # Connect Events
+        self.m_button13.Bind(wx.EVT_BUTTON, self.viewData)
+        self.m_button14.Bind(wx.EVT_BUTTON, self.resetApp)
+        self.m_button15.Bind(wx.EVT_BUTTON, self.addComparison)
+        self.m_button16.Bind(wx.EVT_BUTTON, self.exitApp)
+
     def __del__(self):
         pass
+
+    # Virtual event handlers, override them in your derived class
+    def viewData(self, event):
+        event.Skip()
+
+    def resetApp(self, event):
+        event.Skip()
+
+    def addComparison(self, event):
+        event.Skip()
+
+    def exitApp(self, event):
+        event.Skip()
